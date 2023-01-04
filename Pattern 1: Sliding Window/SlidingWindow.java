18d109ee8baa63f0782d791562ab3e26c6323c12
Sunil Chormale <sunilschormale01@gmail.com>
2023-01-04 08:26:07 +0000

    Sliding Window Creation

---
+++ Pattern 1: Sliding Window/SlidingWindow.java
@@ -0,0 +1,30 @@
+
+public class SlidingWindow{
+
+    public static void main(String[] args){
+        System.out.println("***Sliding Window***");
+        /**
+            Basic of Sliding window
+         */
+
+        int windowSize = 3;
+        int[] arr = {1,2,3,4,5,6,7,8,9};
+
+        /** 
+        We have to initialize the WindowStart and WindowEnd 
+        at the same position as initially we dont have the window ready so 
+        we are going to make it.
+        */
+        int windowStart = 0;
+        //Here we are initializing the WindowEnd in loop itself.
+        for(int windowEnd = 0; windowEnd < arr.length; windowEnd++/*this is also slider*/){
+
+            if(windowEnd >= windowSize-1){ // this means that we are iterating to windowsize to create it like 012 this is first window
+                //Here at this point actual window will be created and then we have to slide that window.
+                System.out.println("Sliding Window: " + windowStart + " " + windowEnd);
+                //this is the start of the window and we are now sliding it forward
+                windowStart++;  
+            }
+        }
+    }
+}
